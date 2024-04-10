05c114e69f315de854dea0b4cf83cdb149752d53
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-04-09 22:29:04 -0500

    added content for main banner, updated styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { init } from "./js/init.js";
 import "./scss/styles.scss";
 import resume from "./assets/resume.pdf";
+import headshot from "./assets/headshot.jpg";
 
 init();
 
@@ -9,3 +10,6 @@ const resumeLink = Array.from(document.querySelectorAll(".resume-link"));
 resumeLink.forEach((button) => {
 	button.href = resume;
 });
+
+const heroImg = document.querySelector("#hero-img");
+heroImg.src = headshot;
