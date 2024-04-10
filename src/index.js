5dc16d24a4b176057a402751587e211345c0584f
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-04-09 22:21:12 -0500

    added link for resume, started styling main banner

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import { init } from "./js/init.js";
 import "./scss/styles.scss";
+import resume from "./assets/resume.pdf";
 
 init();
+
+const resumeLink = Array.from(document.querySelectorAll(".resume-link"));
+
+resumeLink.forEach((button) => {
+	button.href = resume;
+});
