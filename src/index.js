d8d3943b5996410e6804bb7f36d6215176f6caa0
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-04-22 22:05:41 -0500

    added project for portfolio

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import headshot from "./assets/headshot.jpg";
 import library from "./assets/library.png";
 import coffee from "./assets/coffee.png";
 import todo from "./assets/todo.png";
+import weather from "./assets/weather.png";
 
 init();
 
@@ -25,3 +26,6 @@ coffeeImg.src = coffee;
 
 const todoImg = document.querySelector("#to-do-img");
 todoImg.src = todo;
+
+const weatherImg = document.querySelector("#weather-img");
+weatherImg.src = weather;
