706132a7891fd1959e5651506934225667d65168
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-06-11 16:03:56 -0500

    added back to top button and smooth scrolling

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,8 @@
 export { init };
 import { updateHeaderActiveLink } from "./components/header";
+import { scrollToTop } from "./components/toTopBtn";
 
 function init() {
 	updateHeaderActiveLink();
+	scrollToTop();
 }
